09eec8ae055dca7f96ebe56b6c804c49753cd8ed
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-05-16 15:17:47 +0800

    Use prompt to decide if data.json is generated

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,4 @@
-module.exports = api => {
+module.exports = (api, options) => {
   api.extendPackage({
     devDependencies: {
       "markdown-loader": "^5.1.0",
@@ -7,6 +7,11 @@ module.exports = api => {
   })
 
   api.render('./template')
+
+  if (options.dataFile) {
+    api.render('./data')
+  }
+  console.log('dataFile', options.dataFile);
 
   if (api.invoking && api.hasPlugin('eslint')) {
     const { hooks: lint } = require('@vue/cli-plugin-eslint/generator');
---
+++ prompts.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: 'dataFile',
+    type: 'confirm',
+    message: 'Do you want to create @/components/data.json file?',
+    default: false
+  }
+]
